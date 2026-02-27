1954d26e8241a35198186c04f93e8368b8f0c408
Hamza Y. <hamzaprg33@gmail.com>
2026-02-27 12:57:01 +0100

    Add MessageContent intent and update log message

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-require('dotenv').config();
 const { Client, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({
-    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages]
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
 });
 
 client.once('ready', () => {
-    console.log(`Eingeloggt als ${client.user.tag}!`);
+    console.log(`Eingeloggt als ${client.user.tag}`);
 });
 
-// Startet den Bot mit dem Token aus der lokalen .env Datei
+// Nutzt den Token aus deiner lokalen .env Datei
 client.login(process.env.DISCORD_TOKEN);
